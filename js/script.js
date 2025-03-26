ce754af9aed69e7411cb17ca877ee1fcd520da52
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-26 16:47:02 +0100

    [FIX] removed prompt

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 const form = document.querySelector('form');
 const nameField = document.getElementById('name-field');
-const kmField = prompt(document.getElementById('km-field'));
-const ageField = prompt(document.getElementById('age-field'));
+const kmField = document.getElementById('km-field');
+const ageField = document.getElementById('age-field');
 
 // dichiaro la variabile per il prezzo del biglietto
 let biglietto = 0.21 * kmField;
