58786e49b1c4e121e328e92826284461ad4a2f57
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-26 16:31:27 +0100

    [UPD] add js variables for price

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@
 
 const form = document.querySelector('form');
 const nameField = document.getElementById('name-field');
-const kmField = parseInt(prompt(document.getElementById('km-field')));
-const ageField = parseInt(prompt(document.getElementById('age-field')));
+const kmField = document.getElementById('km-field');
+const ageField = prompt(document.getElementById('age-field'));
+
+// dichiaro la variabile per il prezzo del biglietto
+let biglietto = 0.21 * kmField;
+
+console.log(kmField, ageField, biglietto);
+
+// dichiaro la variabile per il prezzo finale
+let finalprice;
+
