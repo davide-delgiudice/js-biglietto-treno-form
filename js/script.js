1c43d2c5cdcd0389d63ced36407e1ad1a5f079d1
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-26 16:21:29 +0100

    [UPD] add js elements

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// richiamo gli elementi che mi servono
+
+const form = document.querySelector('form');
+const nameField = document.getElementById('name-field');
+const kmField = parseInt(prompt(document.getElementById('km-field')));
+const ageField = parseInt(prompt(document.getElementById('age-field')));
