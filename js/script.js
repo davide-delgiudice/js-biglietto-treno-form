0a24400450301fc98944ef204bea891e041ba81e
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-26 16:35:46 +0100

    [UPD] add prompt for km

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const form = document.querySelector('form');
 const nameField = document.getElementById('name-field');
-const kmField = document.getElementById('km-field');
+const kmField = prompt(document.getElementById('km-field'));
 const ageField = prompt(document.getElementById('age-field'));
 
 // dichiaro la variabile per il prezzo del biglietto
